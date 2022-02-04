f2ad2c79fbbd8dfc1612e10e8a3dd87988ef4367
Adil Nehal <65488685+AdilNehal22@users.noreply.github.com>
2022-02-04 15:51:19 +0500

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,4 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const mnemonic = '2559dc4667948dc3da2c84c96a22c5bd0364c04b2f2e796e3734d1dc1d61e711';
-const infuraURL = 'https://rinkeby.infura.io/v3/daf2ac3c93d5408f8ae10fe67c9e9bca';
+
 
 module.exports = {
 
